de4007d8a47a33339c8b22d5ae3e20ae33127e8d
Wandu <d_wandz@yahoo.com>
2019-04-23 22:19:15 +0200

    Added form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, {Component} from 'react'
 import Table from './Table'
 class App extends Component {
+    state = {
+        characters: [],
+    }
+
+    removeCharacter = index => {
+        const {characters} = this.state
+
+        this.setState({
+            characters: characters.filter((characters, i) => {
+                return i !== index
+            }),
+        })
+    }
+
     render() {
+        const {characters} = this.state
         return (
             <div className="container">
-                <Table/>
+                <Table characterData={characters} removeCharacter={this.removeCharacter}/>
             </div>
         )
     }
