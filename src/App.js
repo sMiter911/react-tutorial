791b94f499dfcdba7bd9af4b7442642a15a68608
Wandu <d_wandz@yahoo.com>
2019-04-23 22:33:39 +0200

    create add and delete values

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import Table from './Table'
+import Form from './Form'
 class App extends Component {
     state = {
         characters: [],
@@ -15,11 +16,16 @@ class App extends Component {
         })
     }
 
+    handleSubmit = character => {
+        this.setState({ characters: [...this.state.characters, character] })
+    }
+
     render() {
         const {characters} = this.state
         return (
             <div className="container">
                 <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+                <Form handleSubmit={this.handleSubmit} />
             </div>
         )
     }
